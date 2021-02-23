a5ee35e15b4ff8fe30f8e24d72c1be261b092dc5
A01274966 <A01274966@itesm.mx>
2021-02-22 22:35:39 -0600

    Laboratorio 8 y 9

---
+++ Lab_8/main.js
@@ -0,0 +1,44 @@
+function promedio(list){
+    let sum = 0;
+    for(let i of list){
+        sum += i;
+    }
+    return sum/list.length;
+}
+
+function text(str){
+    const filesystem = require('fs');
+    filesystem.writeFileSync('text.txt', str);
+    return true;
+}
+
+function max(list){
+    let m = list[0];
+    for(let i of list){
+        if(i > m){
+            m = i;
+        }
+    }
+    return m;
+}
+
+const arreglo = [46, 75, 98, 23, 28, 74, 7, 79, 35, 39, 48, 62];
+
+console.log(promedio(arreglo));
+console.log(text("Hola Mundo"));
+console.log(max(arreglo));
+
+let file;
+const fs = require("fs");
+
+'use strict';
+const http = require('http');
+
+const server = http.createServer(function(req, res){
+    fs.readFile('../Lab_5/index.html', function(error, data){ 
+        res.write(data);
+        return res.end();
+     });
+});
+
+server.listen(8000);
